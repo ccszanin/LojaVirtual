92f1ea803f509a8135661a50daa85f3a215aecea
Carla Cristine <carlacs.cristine@gmail.com>
2023-09-04 15:45:37 -0300

    js add to card

--- src/menuCarrinho.js
+++ src/menuCarrinho.js
@@ -1,3 +1,5 @@
+import { catalogo} from "./utilidades";
+
 function abrirCarrinho(){
   document.getElementById('carrinho').classList.add('right-[0px]');
   document.getElementById('carrinho').classList.remove('right-[-360px]');
@@ -14,4 +16,23 @@ const botaoAbrirCarrinho = document.getElementById("abrir-carrinho");
 
 botaoFecharCarrinho.addEventListener("click", fecharCarrinho);
 botaoAbrirCarrinho.addEventListener("click", abrirCarrinho);
+}
+
+export function adicionarAoCarrinho(idProduto){
+  const produto = catalogo.find((p) => p.id === idProduto);
+  const containerProdutosCarrinho = document.getElementById('produtos-carrinho');
+  const cartaoProdutoCarrinho = ` <article class="flex bg-slate-100 rounded-lg p-1 relative"> 
+  <button id="fechar-carrinho" class="absolute top-0 right-2">
+    <i class="fa-solid fa-circle-xmark  text-slate-500 hover:text-slate-800"></i>
+  </button>
+  <img src="./assets/img/${produto.imagem}" alt="Carrinho: ${produto.nome}" class="h-24 rounded-lg">
+  <div class="py-2">
+    <p class="text-slate-900 text-sm">${produto.nome}</p>
+    <p class="text-slate-400 text-xs">Tamanho: M</p>
+    <p class="text-slate-500 tex t-lg">$${produto.preco}</p>
+  </div>
+  
+</article>`;
+
+containerProdutosCarrinho.innerHTML += cartaoProdutoCarrinho;
 }
