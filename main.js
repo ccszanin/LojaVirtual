2f8de177a90932f160d8334798407af119425efd
Carla Cristine <carlacs.cristine@gmail.com>
2023-09-12 21:32:40 -0300

    Filter adjustments

--- main.js
+++ main.js
@@ -1,9 +1,11 @@
 import { renderizarCatalogo } from "./src/cartaoProduto";
+import { inicializarFiltros } from "./src/filtrosCatalogo";
 import { atualizarPrecoCarrinho, inicializarCarrinho, renderizarProdutosCarrinho } from "./src/menuCarrinho";
 
 renderizarCatalogo();
 inicializarCarrinho();
 renderizarProdutosCarrinho();
 atualizarPrecoCarrinho();
+inicializarFiltros();
 
 
